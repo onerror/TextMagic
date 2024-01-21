d2a33e74390f54710c75ca1d38da0db4f4d5e622
Evgeny Pyastolov <pyastolov@gmail.com>
2024-01-21 23:32:22 +0500

    updated readme and initial migration

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # TextMagic test task
+
 A test task for TextMagic
 The task is:
 
@@ -11,7 +12,7 @@ What are questions with fuzzy logic? "2 + 2 ="
 
 The correct responses here will be **1 OR 2 OR (1 AND 2)**. At the same time, any other combinations (for example, **1 AND 3**) will not be considered correct, despite the fact that they include a correct answer.
 What is expected as a result?
-A link to GitHub / Bitbucket with code and instructions for deploying the project. The project should be wrappe  in a
+A link to GitHub / Bitbucket with code and instructions for deploying the project. The project should be wrapped in a
 docker. The user should be able to take the test from beginning to end and see two lists at the end - questions they answered correctly and questions where the answers contained errors. You should be able to take the test as many times as you like. Every test result should be saved in the database (displaying results is not necessary) (Optional) Both the questions and the answers to each question should be shown to the user in a random order for each new series of tests.
 
 Requirements: The task should be completed using Symfony and PostgreSQL. The appearance does not matter, authorization is not needed, admin panel is not needed, it is enough to once add questions with answers to the database.
@@ -24,7 +25,7 @@ To run the project locally after cloning the code just run next command form the
 ```bash
 docker compose build
 ```
-
+this will run migrations to the database as well
 ```bash
 docker compose up
 ```
@@ -37,3 +38,24 @@ then the site will be available at http://localhost
 while the database will be available locally at
 `jdbc:postgresql://localhost:5432/postgres`
 login and password are `test`
+
+# Disclaimer
+
+All credentials are left in github on purpose, it will only work in docker and has only auth data for test purposes
+
+# Ways to improve
+
+- Unit tests. However, so far the app doesn't have much logic to test. Maybe a couple of integration test would be nice
+- Login, auth, admin...
+- The purpose of the app is a little vague, so the database schema would be reworked. For example, if more than 32 answer
+options per question is needed, then all attempt answers would be stored separately in its own table having relations to
+the test_attempt and question tables
+- the same holds for the right answers. If the 'formula style' not needed, and there might be very long 'strings' 
+of possible answers or formulas are not necessary at all, then another schema would be used, for example, 
+we might have a table called right_answers having int (for 32 answer options) as a value and a relation to the question table
+or if 32 (integer) is not enough then we would have the table right_answer having relations not only to the question table but to the answer_variant table as well.
+- a method to break a test attempt
+- calculating scores
+- displaying right and wrong answers, not only questions
+- if questions are typical and might be shared in many tests then it would be reasonable to add another table between 
+a question and a test, their relation would become many to many. The same holds true for questions and answers 
--- textmagic/migrations/Version20240120184070.php
+++ textmagic/migrations/Version20240120184070.php
@@ -23,7 +23,7 @@ final class Version20240120184070 extends AbstractMigration
         $this->addSql("ALTER SEQUENCE test_id_seq RESTART WITH 1;");
         $this->addSql("ALTER SEQUENCE question_id_seq RESTART WITH 1;");
         $this->addSql("ALTER SEQUENCE answer_variant_id_seq RESTART WITH 1;");
-
+        
         $this->addSql(
             "INSERT INTO public.customer (id, surname, login, first_name) VALUES (NEXTVAL('customer_id_seq'), 'Tестов', 'test', 'Тест');"
         );
@@ -56,7 +56,7 @@ final class Version20240120184070 extends AbstractMigration
             "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 2, '0', 'x3');"
         );
         
-        //3
+        // 3
         $this->addSql(
             "INSERT INTO public.question (id, title, right_answer_formula, test_id) VALUES (NEXTVAL('question_id_seq'), '3 + 3 =', '1,4,8,5,9,12,13', 1);"
         );
@@ -73,7 +73,7 @@ final class Version20240120184070 extends AbstractMigration
             "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 3, '2 + 4', 'x4');"
         );
         
-        //4
+        // 4
         $this->addSql(
             "INSERT INTO public.question (id, title, right_answer_formula, test_id) VALUES (NEXTVAL('question_id_seq'), '4 + 4 =', '1,8,9', 1);"
         );
@@ -90,6 +90,107 @@ final class Version20240120184070 extends AbstractMigration
             "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 4, '0 + 8', 'x4');"
         );
         
+        // 5
+        $this->addSql(
+            "INSERT INTO public.question (id, title, right_answer_formula, test_id) VALUES (NEXTVAL('question_id_seq'), '5 + 5 =', '4', 1);"
+        );
+        $this->addSql(
+            "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 5, '6', 'x1');"
+        );
+        $this->addSql(
+            "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 5, '18', 'x2');"
+        );
+        $this->addSql(
+            "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 5, '10', 'x3');"
+        );
+        $this->addSql(
+            "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 5, '9', 'x4');"
+        );
+        $this->addSql(
+            "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 5, '0', 'x5');"
+        );
+        
+        // 6
+        $this->addSql(
+            "INSERT INTO public.question (id, title, right_answer_formula, test_id) VALUES (NEXTVAL('question_id_seq'), '6 + 6 =', '8,16,24', 1);"
+        );
+        $this->addSql(
+            "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 6, '3', 'x1');"
+        );
+        $this->addSql(
+            "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 6, '9', 'x2');"
+        );
+        $this->addSql(
+            "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 6, '0', 'x3');"
+        );
+        $this->addSql(
+            "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 6, '12', 'x4');"
+        );
+        $this->addSql(
+            "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 6, '5 + 7', 'x5');"
+        );
+        
+        // 7
+        $this->addSql(
+            "INSERT INTO public.question (id, title, right_answer_formula, test_id) VALUES (NEXTVAL('question_id_seq'), '7 + 7 =', '2', 1);"
+        );
+        $this->addSql(
+            "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 7, '5', 'x1');"
+        );
+        $this->addSql(
+            "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 7, '14', 'x2');"
+        );
+        
+        // 8
+        $this->addSql(
+            "INSERT INTO public.question (id, title, right_answer_formula, test_id) VALUES (NEXTVAL('question_id_seq'), '8 + 8 =', '1', 1);"
+        );
+        $this->addSql(
+            "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 8, '16', 'x1');"
+        );
+        $this->addSql(
+            "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 8, '12', 'x2');"
+        );
+        $this->addSql(
+            "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 8, '9', 'x3');"
+        );
+        $this->addSql(
+            "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 8, '5', 'x4');"
+        );
+        
+        // 9
+        $this->addSql(
+            "INSERT INTO public.question (id, title, right_answer_formula, test_id) VALUES (NEXTVAL('question_id_seq'), '9 + 9 =', '1,4,8,5,9,12,13', 1);"
+        );
+        $this->addSql(
+            "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 9, '18', 'x1');"
+        );
+        $this->addSql(
+            "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 9, '9', 'x2');"
+        );
+        $this->addSql(
+            "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 9, '17 + 1', 'x3');"
+        );
+        $this->addSql(
+            "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 9, '2 + 16', 'x4');"
+        );
+        
+        // 10
+        $this->addSql(
+            "INSERT INTO public.question (id, title, right_answer_formula, test_id) VALUES (NEXTVAL('question_id_seq'), '10 + 10 =', '8', 1);"
+        );
+        $this->addSql(
+            "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 10, '0', 'x1');"
+        );
+        $this->addSql(
+            "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 10, '2', 'x2');"
+        );
+        $this->addSql(
+            "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 10, '8', 'x3');"
+        );
+        $this->addSql(
+            "INSERT INTO public.answer_variant (id, question_id, title, alias) VALUES (NEXTVAL('answer_variant_id_seq'), 10, '20', 'x4');"
+        );
     }
     
     public function down(Schema $schema): void
